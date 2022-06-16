5b8ec3bd0853275a66c28b8331cc2142eeb3e79b
JaehonDev <println@kakao.com>
2022-06-16 16:35:19 +0900

    display breakpoint

---
+++ components/DisplayBreakpoint.vue
@@ -0,0 +1,24 @@
+<!-- Vue Component -->
+
+<template>
+  <v-card :height="height">
+      aaaa
+  </v-card>
+</template>
+
+<script>
+export default {
+  computed: {
+    height () {
+      console.log(1);
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return 220
+        case 'sm': return 400
+        case 'md': return 500
+        case 'lg': return 600
+        case 'xl': return 800
+      }
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
- <Accessbility/>
+  <div>
+    <Accessbility/>
+    <display-breakpoint></display-breakpoint>
+    <v-dialog :fullscreen="$vuetify.breakpoint.mobile">
+      111
+    </v-dialog>
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data: () => ({ isMobile: false }),
+
+  beforeDestroy () {
+    if (typeof window === 'undefined') return
+
+    window.removeEventListener('resize', this.onResize, { passive: true })
+  },
+
+  mounted () {
+    this.onResize()
+
+    window.addEventListener('resize', this.onResize, { passive: true })
+  },
+
+  methods: {
+    onResize () {
+      this.isMobile = window.innerWidth < 600
+    },
+  },
 }
 </script>
